8a6ebd4ea498caca5351d3f26cd1f3eb8b84ab26
Sollay <mayusos@live.it>
2021-08-22 16:22:36 +0200

    Add files via upload

---
+++ new animations/137/script.js
@@ -0,0 +1,499 @@
+
+//Page 137 - Sburb loading
+let SburbLoadingStrings = [
+  "Accelerazione Cinghie",
+  "Progettazione Impianti",
+  "Protezione Pentole",
+  "Accesso Tubi di Scarico",
+  "Correlazione Tegole",
+  "Unificazione Mazze",
+  "Qualificazione Ascensori Elettrostatici",
+  "Condivisione Mollette",
+  "Acquisizione Fluidi Circolari",
+  "Classificazione Spessori Fondamenta",
+  "Percezione Mensole",
+  "Creazione Space Networks",
+  "Trasformazione Trapani",
+  "Regolazione Scorte",
+  "Selezione Imbottitura Spioventi",
+  "Determinazione Condotti",
+  "Rappresentazione Cemento",
+  "Ricostruzione di Stabilizzatori",
+  "Sostenimento Ciminiere",
+  "Utilizzo Perni",
+  "Riconciliazione Artefatti",
+  "Alterazione Carrucole",
+  "Spargimento Filtri Spaziali",
+  "Caratterizzazione Guarniture Girevoli",
+  "Manifestazione Giunture",
+  "Resettamento Morse Supplementari",
+  "Professazione Strisce",
+  "Intarsiazione Livellatrici di Materia",
+  "Discussione Driver",
+  "Formazione Margini di Montaggio",
+  "Delineamento Spazi Vuoti",
+  "Pressione Interruttori di Accensione",
+  "Riscrittura Controlli",
+  "Riproduzione Tunnel",
+  "Inventariazione bottoni",
+  "Sopportazione Impugnature Travetti",
+  "Apertura Porte",
+  "Rimozione Innesti",
+  "Abbandono Condotti di Vapore",
+  "Definizione Prese",
+  "Calcolo Riscaldamenti",
+  "Sollevamento Reti",
+  "Amministrazione Piastrelle",
+  "Misurazione Risorse",
+  "Installazione Inneschi Remoti",
+  "Estrazione Angoli",
+  "Lavorazione Ventilatori",
+  "Delegazione Console",
+  "Trattamento Pietre di Montaggio",
+  "Attuazione Deflettori Maschere",
+  "Intensificazione Vicoli",
+  "Improvvisazione Contenuti",
+  "Individuazione Caschetti",
+  "Prescrizione Opere Arcuate",
+  "Strutturazione Serraggi di Metallo",
+  "Simbolizzazione Torni",
+  "Attivazione Kit di Piombini",
+  "Adattamento Rivestimenti",
+  "Riparazione Canali",
+  "Pianificazione Compressori",
+  "Richiesta Grucce",
+  "Ristrutturazione Coclee delle Serrature",
+  "Rimozione Hardware",
+  "Collezione Fulmini Alternati",
+  "Mantenimento Regolatori Ondulati",
+  "Affilamento Ghiere",
+  "Conduzione Chiodi",
+  "Comparazione Assetti",
+  "Compilazione Sigillanti",
+  "Completamento Percorsi",
+  "Composizione Ingranaggi",
+  "Calcolo Ammortizzatori",
+  "Concepimento Trattamento Elettrostatico",
+  "Ordinazione Grate di Copiglie",
+  "Organizzazione Cravatte",
+  "Orientazione Scale",
+  "Superamento Materiali",
+  "Ottimizzazione Termocoppie",
+  "Dimostrazione Scorte di Smeriglio",
+  "Espansione Fondo Serrature",
+  "Preparazione Mastici di Guardaroba",
+  "Superamento Chiusure",
+  "Semplificazione Ancoraggi",
+  "Navigazione Fonti",
+  "Perfezionamento Tenditori",
+  "Verifica Chiodi di Cancelli",
+  "Risoluzione Ascensori Aritmetici",
+  "Negoziazione Sbocchi",
+  "Normalizzazione Lamelle",
+  "Costruzione Vaporizzatori Superficiali",
+  "Controllo Torce",
+  "Lavorazione Tritacarni",
+  "Livellamento Pialle",
+  "Compensazione Forme",
+  "Acquisizione Lampadine",
+  "Adozione Rivetti",
+  "Osservazione Viali",
+  "Accertamento Cavi Coassiali",
+  "Lancio Argani",
+  "Istituzione Generatori di Controcircuiti",
+  "Istruzione Stoppini",
+  "Integrazione Tapparelle",
+  "Interpretazione Legname",
+  "Clarificazione Nastri",
+  "Classificazione Pezzi di Legno",
+  "Chiusura Ingranaggi",
+  "Catalogazione Strisce di Materia",
+  "Tracciamento Conduttori",
+  "Concettualizzazione Terminali",
+  "Stimulazione Supporti",
+  "Rovesciamento Distanziatori di Perni",
+  "Surgelamento Connettori",
+  "Sciogliemento Agganci al Terreno",
+  "Analisi Occhi",
+  "Anticipazione Portali",
+  "Controllo Rulli",
+  "Conversione Angoli di Potenza",
+  "Coordinazione Pinzatrici",
+  "Correzione Piegatrici",
+  "Consulenza Guarnizione Travi",
+  "Rilevamento Tubi Grondaia",
+  "Reclutamento Scarichi",
+  "Riabilitazione Puntoni",
+  "Rinforzamento Lavatrici",
+  "Segnalazione Valvole Protettive",
+  "Denominazione Materozze",
+  "Designazione Anelli",
+  "Rilevamento Cinghie",
+  "Moltiplicazione Chiodatori",
+  "Elaborazione Circuiti di Tubi",
+  "Dramatizzazione Flange",
+  "Suddivisione Composti",
+  "Riparazione Steli",
+  "Intreccio Sindacati",
+  "Piazzamento Rubinetti",
+  "Smistamento Slot Filo",
+  "Fissaggio Tronchesi",
+  "Deviazione Piastre Catarsi",
+  "Procurazione Limiti Portata",
+  "Transferimento Filo Sillogistico",
+  "Orientamento Interruttore di Follia",
+  "Rinvio Spole Temporali",
+  "Diagnosi Pomelli",
+  "Esplorazione Lucchetti",
+  "Somministrazione Cerniere",
+  "Visualizzazione Fermi",
+  "Reinvio Leganti Arcuati",
+  "Rigenerazione Scanalature",
+  "Adeguamento Portali Estetici",
+  "Ricerca Riserve",
+  "Rimpicciolimento Cunicoli",
+  "Assemblamento Blocchi",
+  "Valutazione Colla",
+  "Ottenimento Scatole",
+  "Verifica Passaggi di Nescienza",
+  "Conservazione Colpi",
+  "Costruzione Supporti",
+  "Contrazione Seghe",
+  "Disposizione Ferri di Istansiazione",
+  "Riconoscimento Flussi",
+  "Consolidamento Pinze",
+  "Mappatura Spessori",
+  "Revisione Bistecche",
+  "Programmazione Ritardi Dischi",
+  "Semplificazione Montacarichi",
+  "Progettazione Livelli",
+  "Rafforzamento Cavità",
+  "Creazione Blocchi",
+  "Stima Adesivi",
+  "Valutazione Cemento",
+  "Esaminazione Tenditori",
+  "Elaborazione Vaporizzatori",
+  "Manutensione Viali",
+  "Trascrizione Chiodi",
+  "Revisione Comandi",
+  "Separazione Forme Assolute",
+  "Preventivazione Tubetti in Lamiera",
+  "Preparazione Divise",
+  "Ridefinizione Perni Cartesiani",
+  "Pitturazione Chiusure",
+  "Presentazione Ganci",
+  "Finalizzazione Trivelle",
+  "Formulazione Giunti",
+  "Elaborazione Tubi di Scappamento",
+  "Imaginazione Materiali",
+  "Induzione Serrande",
+  "Influenzamento Ruote",
+  "Autorizzazione delle Guarniture",
+  "Sollevamento Martelli",
+  "Svuotamento Cricchetti",
+  "Sovrapposizione Ventilatori",
+  "Prioritizzazione Soffitti Cardinali",
+  "Specificazione Elementi Aggregati",
+  "Sistemizzazione Collanti",
+  "Definizione Tasche",
+  "Pubblicizzazione Eteri Remoti",
+  "Riduzione Fermi Scanalature",
+  "Disciplinazione Archetipi Oscuri",
+  "Monitorizzazione Assetti",
+  "Lancio Sistemi di Manifestazione",
+  "Transformazione Soffitti",
+  "Riorganizzazione Chiavi",
+  "Formalizzazione Giunti di Immersione",
+  "Giustificazione Estrattori di Colpi",
+  "Consultazione Aggregati",
+  "Gestione Gomiti",
+  "Revisione Collegamenti",
+  "Realizzazione Levatrici di Alluminio",
+  "Celebrazione Dischi",
+  "Esibizione Rubinetti Assoluti",
+  "Progressione Bobine Idranti",
+  "Approssimazione Riflettori",
+  "Informazione Ruote",
+  "Invenzione Ascensori di Gomma",
+  "Esecuzione Chiavi Inglesi",
+  "Valutazione Raccordi di Gesso",
+  "Potensiamento Vie di Accensione",
+  "Rinnovazione Intermittenze",
+  "Raccomandazione Cricchetti",
+  "Approvazione Barriere",
+  "Spazzamento Cariche Impatto",
+  "Lavorazione Specchi",
+  "Dettaglio Raccoglitori",
+  "Applicazione Provvedimenti",
+  "Distribuzione Sistemi",
+  "Presentazione Prese",
+  "Concatenazione Registri",
+  "Sperimentazione Diffusori di Polvere",
+  "Raccoglimento Manovelle",
+  "Fornitura Sacche da Gronda",
+  "Effettuazione Fermate di Scorrimenti"
+];
+
+window.onload = function() {
+  var Video = document.getElementById("Video");
+  
+  /* STRINGS (translate as needed) */
+
+  var HiQualityStr = "QUALITÀ ALTA";
+  var LoQualityStr = "QUALITÀ BASSA";
+
+  // Prefix for images & stuff
+  //var Prefix = "https://homestuckITA.github.io/new animations/general/";
+  var Prefix = "https://homestuckITA.github.io/new animations/general/";
+
+  /* -------- */
+
+  Video.style.position = "absolute";
+  Video.style.left = "0";
+
+  var Audio = document.getElementById("Audio");
+  var Hitsound = document.getElementById("Hitsound");
+  var InteractDiv = document.getElementsByClassName("interact")[0];
+
+  var LoadingState = 0;
+
+  var PlayAgain = document.createElement("img");
+  PlayAgain.setAttribute("src", Prefix+"arrow.png");
+  PlayAgain.setAttribute("id", "replay");
+
+  var Volume = document.createElement("img");
+  Volume.setAttribute("src", Prefix+"volumefull.png");
+  Volume.setAttribute("id", "volume");
+  var VolumeState = 0;
+
+  var LoadingScreen = document.createElement("img");
+  LoadingScreen.setAttribute("style", "position:absolute; top:0px; left:0px;");
+  LoadingScreen.setAttribute("src", Prefix+"loadingbg.gif");
+
+  InteractDiv.appendChild(LoadingScreen);
+
+  var LoadingButton = document.createElement("img");
+  LoadingButton.setAttribute("src", Prefix+"start.png");
+  LoadingButton.setAttribute("style", "position:absolute; top:365px; left:220.5px;cursor:pointer;");
+
+  var LoadingProgressVideo = document.createElement("p");
+  LoadingProgressVideo.setAttribute("style", "position:absolute; top:370px; left:0; width:100%; color:black; text-align:center;");
+
+  var LoadingProgressAudio = document.createElement("p");
+  LoadingProgressAudio.setAttribute("style", "position:absolute; top:390px; left:0; width:100%; color:black; text-align:center;");
+
+  var HiButton = document.createElement("p");
+  HiButton.setAttribute("style", "position:absolute; top:370px; left:0; width:100%; color:black; text-align:center; font-size:25px; text-decoration:underline; cursor:pointer;");
+  HiButton.innerHTML = HiQualityStr;
+
+  var LoButton = document.createElement("p");
+  LoButton.setAttribute("style", "position:absolute; top:400px; left:0; width:100%; color:black; text-align:center; font-size:25px; text-decoration:underline; cursor:pointer;");
+  LoButton.innerHTML = LoQualityStr;
+
+  SburbLoading = document.createElement("p");
+  SburbLoading.setAttribute("style", "position:absolute; top:390px; left:153px; width:339px; height:16px; color:white; text-align:center; font-family: 'Times New Roman', serif;");
+
+  /* LOADING SUBVIDEOS */
+
+  var AudioRequest = new XMLHttpRequest();
+  var VideoRequest = new XMLHttpRequest();
+
+  var VideoBlob, AudioBlob;
+  // detect if we have the lo-quality option
+
+  if(Video.getAttribute("lofilesrc") != null) {
+    // show the option
+    HiButton.addEventListener("click", ()=>{
+      var AudioURL = Audio.getAttribute("filesrc");
+      var VideoURL = Video.getAttribute("filesrc");
+      AudioRequest.open('GET', AudioURL, true);
+      VideoRequest.open('GET', VideoURL, true);
+      AudioRequest.responseType = "blob";
+      VideoRequest.responseType = "blob";
+      AudioRequest.send();
+      VideoRequest.send();
+      InteractDiv.removeChild(HiButton);
+      InteractDiv.removeChild(LoButton);
+      InteractDiv.appendChild(LoadingProgressVideo);
+      InteractDiv.appendChild(LoadingProgressAudio);
+    })
+
+    LoButton.addEventListener("click", ()=>{
+      var AudioURL = Audio.getAttribute("filesrc");
+      var VideoURL = Video.getAttribute("lofilesrc");
+      AudioRequest.open('GET', AudioURL, true);
+      VideoRequest.open('GET', VideoURL, true);
+      AudioRequest.responseType = "blob";
+      VideoRequest.responseType = "blob";
+      AudioRequest.send();
+      VideoRequest.send();
+      InteractDiv.removeChild(HiButton);
+      InteractDiv.removeChild(LoButton);
+      InteractDiv.appendChild(LoadingProgressVideo);
+      InteractDiv.appendChild(LoadingProgressAudio);
+    })
+
+    /* Probe size */
+
+    var HiSizeReq = new XMLHttpRequest();
+    var LoSizeReq = new XMLHttpRequest();
+
+    HiSizeReq.open('HEAD', Video.getAttribute("filesrc"), true);
+    LoSizeReq.open('HEAD', Video.getAttribute("lofilesrc"), true);
+
+    HiSizeReq.send();
+    LoSizeReq.send();
+
+    HiSizeReq.addEventListener("load", ()=> {
+      var FSize = Math.ceil(HiSizeReq.getResponseHeader('content-length') / 1024 / 1024 * 100)/100;
+      HiButton.innerText += " (" + FSize + " MB)";
+    });
+
+    LoSizeReq.addEventListener("load", ()=> {
+      var FSize = Math.ceil(LoSizeReq.getResponseHeader('content-length') / 1024 / 1024*100)/100;
+      LoButton.innerText += " (" + FSize + " MB)";
+    });
+
+    InteractDiv.appendChild(HiButton);
+    InteractDiv.appendChild(LoButton);
+    
+  } else {
+    var AudioURL = Audio.getAttribute("filesrc");
+    var VideoURL = Video.getAttribute("filesrc");
+
+
+    AudioRequest.open('GET', AudioURL, true);
+    VideoRequest.open('GET', VideoURL, true);
+    AudioRequest.responseType = "blob";
+    VideoRequest.responseType = "blob";
+    AudioRequest.send();
+    VideoRequest.send();
+
+    InteractDiv.appendChild(LoadingProgressVideo);
+    InteractDiv.appendChild(LoadingProgressAudio);
+  }
+
+  AudioRequest.addEventListener("progress", (event)=>{
+    if(event.lengthComputable) {
+      LoadingProgressAudio.innerHTML = `Audio: ${Math.ceil(event.loaded/event.total * 100)}% (${event.loaded}/${event.total})`;
+    } else {
+      LoadingProgressAudio.innerHTML = `Audio: ??%`
+    }
+  }, false);
+
+  VideoRequest.addEventListener("progress", (event)=>{
+    if(event.lengthComputable) {
+      LoadingProgressVideo.innerHTML = `Video: ${Math.ceil(event.loaded/event.total * 100)}% (${event.loaded}/${event.total})`;
+    } else {
+      LoadingProgressVideo.innerHTML = `Video: ??%`
+    }
+  }, false);
+
+  AudioRequest.addEventListener("load", (event)=>{
+      var Blob = AudioRequest.response;
+      AudioBlob = URL.createObjectURL(Blob);
+      LoadingState++;
+  }, false);
+
+  VideoRequest.addEventListener("load", (event)=>{
+      var Blob = VideoRequest.response;
+      VideoBlob = URL.createObjectURL(Blob);
+      LoadingState++;
+  }, false);
+
+  
+
+  var CheckLoadInterval = window.setInterval(CheckLoadStatus, 250);
+
+  function CheckLoadStatus() {
+    if(LoadingState >= 2) {
+      window.clearInterval(CheckLoadInterval);
+      Video.src = VideoBlob;
+      Audio.src = AudioBlob;
+      InteractDiv.removeChild(LoadingProgressVideo);
+      InteractDiv.removeChild(LoadingProgressAudio);
+      InteractDiv.appendChild(LoadingButton);
+      LoadingButton.setAttribute("class", "startvideo");
+      LoadingButton.addEventListener("click", function(){
+        InteractDiv.removeChild(LoadingButton);
+        InteractDiv.removeChild(LoadingScreen);
+        InteractDiv.appendChild(Volume);
+        InteractDiv.appendChild(SburbLoading);
+        Audio.currentTime = 0;
+        Video.currentTime = 0;
+        Audio.play();
+        Video.play();
+      });
+    }
+  }
+
+  PlayAgain.addEventListener("click", function( event ) {
+    Video.currentTime = 0;
+    Video.play();
+    Audio.currentTime = 0;
+    Audio.play();
+    InteractDiv.appendChild(SburbLoading);
+    SburbLoadingEnded = false;
+    InteractDiv.removeChild(PlayAgain);
+    SburbStringIterator = -1;
+  });
+
+  function AudioEnd() {
+    InteractDiv.appendChild(PlayAgain);
+  }
+
+  function VideoEnd() {
+    //InteractDiv.appendChild(PlayAgain);
+  }
+
+  let SburbStringIterator = -1;
+  let SburbLoadingEnded = false;
+  function VideoCheckProgress() {
+    let Delta = 0.0;
+    if(Video.currentTime - Delta > 0.5) {
+      SburbStringIterator++;
+      if(SburbStringIterator >= SburbLoadingStrings.length) {
+        SburbStringIterator = 0;
+      }
+      SburbLoading.innerHTML = SburbLoadingStrings[SburbStringIterator];
+      Delta = Video.currentTime;
+    }
+    if(Video.currentTime > 47.5 && !SburbLoadingEnded) {
+      InteractDiv.removeChild(SburbLoading);
+      SburbLoadingEnded = true;
+    }
+    if(Video.currentTime > 51.8) {
+      Video.currentTime = 49.9;
+    }
+  }
+
+  Audio.addEventListener("ended", AudioEnd);
+
+  Video.addEventListener("ended", VideoEnd);
+  Video.addEventListener("timeupdate", VideoCheckProgress);
+
+  Volume.addEventListener("click", function( event ) {   
+    VolumeState++;
+    if(VolumeState > 3) {
+      VolumeState = 0;
+    }
+    switch(VolumeState) {
+      case 0: //Full
+        Volume.setAttribute("src", Prefix+"volumefull.png");
+        Audio.volume = 1;
+        break;
+      case 1: //67%
+        Volume.setAttribute("src", Prefix+"volumemute.png");
+        Audio.volume = 0;
+        break;
+      case 2: //33%
+        Volume.setAttribute("src", Prefix+"volume33.png");
+        Audio.volume = 0.33;
+        break;
+      case 3: //Muted
+        Volume.setAttribute("src", Prefix+"volume67.png");
+        Audio.volume = 0.67;
+        break;
+    }
+  });
+}
